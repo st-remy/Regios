5dd0fa23ec42f231a54daa6d14b7672c9d94a32f
Adamki11s <adam@adamki11s.co.uk>
2011-09-03 12:47:06 +0100

    Finalised Bukkit Permissions Fallback Support

--- src/couk/Adamki11s/Regios/Permissions/PermissionsCore.java
+++ src/couk/Adamki11s/Regios/Permissions/PermissionsCore.java
@@ -10,69 +10,84 @@ import com.nijiko.permissions.PermissionHandler;
 import couk.Adamki11s.Regios.Regions.Region;
 
 public class PermissionsCore {
-	
+
 	public static PermissionHandler permissionHandler = null;
-    public static boolean hasPermissions = false, iConomyEnabled = false;
-    
-    public static boolean doesHaveNode(Player p, String node){
-    	return (hasPermissions ? (permissionHandler.has(p, node) || p.isOp()) : (p.isOp()));
-    }
-    
-    public static void sendInvalidPerms(Player p){
-    	p.sendMessage(ChatColor.RED + "[Regios] You do not have permissions to do this!");
-    }
-    
-    
-    public static void sendInvalidPermsPopup(SpoutPlayer p){
-    	p.sendNotification("Permissions", ChatColor.RED + "You cannot do this!", Material.FIRE);
-    }
-    
-    public static boolean canModifyBasic(Region r, Player p){
-    	if(doesHaveNode(p, ("regios.override." + r.getName())) || doesHaveNode(p, "regios.override.all")){
-    		return true;
-    	}
-    	if(canModifyMain(r, p)){ return true; }
-    	if(r.getOwner().equals(p.getName())){ return true; }
-    	for(String s : r.getSubOwners()){
-    		if(s.equals(p.getName())){
-    			return true;
-    		}
-    	}
-    	if(p.isOp()){ return true; }
-    	return false;
-    }
-    
-    public static boolean canModifyMain(Region r, Player p){
-    	if(doesHaveNode(p, ("regios.override." + r.getName())) || doesHaveNode(p, "regios.override.all")){
-    		return true;
-    	}
-    	if(r.getOwner().equals(p.getName())){ return true; }
-    	if(p.isOp()){ return true; }
-    	return false;
-    }
-    
-    public static void addUserPermission(Player p, String node){
+	public static boolean hasPermissions = false, iConomyEnabled = false;
+
+	public static boolean doesHaveNode(Player p, String node){
+    	if(p.isOp()){ return true;}
+    	
     	if(hasPermissions){
-    		permissionHandler.addUserPermission(p.getWorld().getName(), p.getName(), node);
-    		permissionHandler.save(p.getName());
-    		try {
+    		return permissionHandler.has(p, node);
+    	} else {
+    		return p.hasPermission(node);
+    	}
+    }
+
+	public static void sendInvalidPerms(Player p) {
+		p.sendMessage(ChatColor.RED + "[Regios] You do not have permissions to do this!");
+	}
+
+	public static void sendInvalidPermsPopup(SpoutPlayer p) {
+		p.sendNotification("Permissions", ChatColor.RED + "You cannot do this!", Material.FIRE);
+	}
+
+	public static boolean canModifyBasic(Region r, Player p) {
+		if (doesHaveNode(p, ("regios.override." + r.getName())) || doesHaveNode(p, "regios.override.all")) {
+			return true;
+		}
+		if (canModifyMain(r, p)) {
+			return true;
+		}
+		if (r.getOwner().equals(p.getName())) {
+			return true;
+		}
+		for (String s : r.getSubOwners()) {
+			if (s.equals(p.getName())) {
+				return true;
+			}
+		}
+		if (p.isOp()) {
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean canModifyMain(Region r, Player p) {
+		if (doesHaveNode(p, ("regios.override." + r.getName())) || doesHaveNode(p, "regios.override.all")) {
+			return true;
+		}
+		if (r.getOwner().equals(p.getName())) {
+			return true;
+		}
+		if (p.isOp()) {
+			return true;
+		}
+		return false;
+	}
+
+	public static void addUserPermission(Player p, String node) {
+		if (hasPermissions) {
+			permissionHandler.addUserPermission(p.getWorld().getName(), p.getName(), node);
+			permissionHandler.save(p.getName());
+			try {
 				permissionHandler.load();
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-    	}
-    }
-    
-    public static void removeUserPermission(Player p, String node){
-    	if(hasPermissions){
-    		permissionHandler.removeUserPermission(p.getWorld().getName(), p.getName(), node);
-    		permissionHandler.save(p.getName());
-    		try {
+		}
+	}
+
+	public static void removeUserPermission(Player p, String node) {
+		if (hasPermissions) {
+			permissionHandler.removeUserPermission(p.getWorld().getName(), p.getName(), node);
+			permissionHandler.save(p.getName());
+			try {
 				permissionHandler.load();
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-    	}
-    }
+		}
+	}
 
 }

9339c5524458f5292efd03224e126a74cad0d4f5
Ole Christian Tvedt <ole.christian.tvedt@gmail.com>
2011-12-23 12:39:17 +0100

    Create new Accounts on every call to avoid errors on missing iCon6.

--- src/couk/Adamki11s/Regios/Economy/iConomyManager.java
+++ src/couk/Adamki11s/Regios/Economy/iConomyManager.java
@@ -7,10 +7,10 @@ import couk.Adamki11s.Regios.Regions.Region;
 import com.iCo6.system.Accounts;
 
 public class iConomyManager extends Buying {
-	private static final Accounts accounts = new Accounts();
-	
+
 	@SuppressWarnings("static-access")
 	private double getBalance(Player p){
+		Accounts accounts = new Accounts();
 		return accounts.get(p.getName()).getHoldings().getBalance();
 	}
 	
@@ -20,6 +20,7 @@ public class iConomyManager extends Buying {
 	
 	@SuppressWarnings("static-access")
 	public void buyRegion(Region r, String buyer, String seller, int price){
+		Accounts accounts = new Accounts();
 		accounts.get(seller).getHoldings().add(price);
 		accounts.get(buyer).getHoldings().subtract(price);
 		super.buy(seller, buyer, r, price);
